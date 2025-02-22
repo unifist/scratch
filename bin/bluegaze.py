7e16d0e40128d74f8b7ca94bdd09703e35118a54
Gaffer Fitch <gaffer.fitch@gmail.com>
2025-02-21 19:08:57 -0500

    Added some fun scratchings

--- bin/bluegaze.py
+++ bin/bluegaze.py
@@ -12,22 +12,67 @@ followers = client.get_followers(actor=profile.handle)
 for follower in followers.followers:
     print(follower.handle)
 
+
+post_objects = client.get_author_feed(actor=profile.handle, limit=3).feed
+posts = []
+for post in post_objects:
+    record = post.post.record
+    subject = getattr(record, "subject", None)
+
+    replies = client.get_post_thread(uri=post.post.uri).thread.replies
+
+    for reply in replies:
+        print(f"Reply: {reply.post.record.text}")
+
+
+
+"""
+
+
+    print(
+        {
+            "uri": post.post.uri,
+            "did": post.post.author.did,
+            "created_at": record.created_at,
+            "root_uri": reply.root.uri if reply else None,
+            "parent_uri": reply.parent.uri if reply else None,
+            "subject_uri": subject.uri if subject else None,
+            "card_link": getattr(record, "external_uri", None),
+            "like_count": post.post.like_count,
+            "reply_count": post.post.reply_count,
+            "repost_count": post.post.repost_count,
+            "labels": post.post.author.labels,
+            "text": record.text,
+            # "images": record.embed. if record.embed else [],
+        }
+    )
+
 response = client.app.bsky.feed.get_author_feed(params={
     "actor": "gaf3.com",
-    "limit": 30,
+    "limit": 5,
     "filter": "posts_with_replies"
 })
 
-def post_to_dict(post):
-
-    value = {
-        "cid": view.post.cid,
-        "author": view.post.author.handle,
-        "created_at": view.post.record.created_at,
-        "text": view.post.record.text
-    }
-
-    return value
-
 for view in response.feed:
-    print(json.dumps(post_to_dict(view.post), indent=2, sort_keys=True), "\n\n")
+
+    print(view.post)
+
+    if hasattr(view.post.record, "reply"):
+        print(view.post.record.reply)
+
+        print(view.post.record.reply.parent.uri)
+
+        client.get_post(
+            uri=view.post.record.reply.parent.uri,
+        )
+
+        response = client.get_likes(
+            uri=view.post.uri,
+            limit=10
+        )
+
+    for like in response.likes:
+        print(like.actor.handle)
+
+
+"""
